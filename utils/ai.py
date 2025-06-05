1ae48a7531e768441678fd701424323a9e4b58d4
lucian-xinitrc <cusmirlucian@gmail.com>
2025-06-05 14:30:48 +0000

    AI

--- utils/ai.py
+++ utils/ai.py
@@ -35,7 +35,7 @@ class ArtificialIntelligence():
 			bot = gethonis.Gethonis("TEST", "gethonis", False, "http://46.202.141.49:8000")
 			async with ctx.channel.typing():
 				response = await asyncio.to_thread(bot.get_message, message)
-			await ctx.send(response)
+				await ctx.send(response)
 		except Exception as e:
 			await ctx.send(e)
 	
