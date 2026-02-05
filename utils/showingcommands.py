6e9c0252e572e0dfd523ad728fe0b34e7a924d05
cusmirlucian@gmail.com <cusmirlucian@gmail.com>
2026-02-05 13:05:16 +0000

    [ showingcommands ] - setting the api IP as domain got a new frontend

--- utils/showingcommands.py
+++ utils/showingcommands.py
@@ -21,7 +21,7 @@ class ShowingCommands():
 		try:
 			if ctx.author.id == 1135659932000202942 or ctx.author.id == 1027255470429319228:
 				data = { "headers": sys_token, "command": "start" }
-				response = requests.post('http://91.99.202.74.com:8888/api/insertCommand', json=data)
+				response = requests.post('http://91.99.202.74:8888/api/insertCommand', json=data)
 				result = response.json()
 				if result['status'] == "started":
 					await ctx.response.send_message("Server Started")
@@ -40,7 +40,7 @@ class ShowingCommands():
 			
 			if ctx.author.id == 1135659932000202942 or ctx.author.id == 1027255470429319228:
 				data = { "headers": sys_token, "command": command, "type": type }
-				response = requests.post('http://91.99.202.74.com:8888/api/insertCustomCommand', json=data)
+				response = requests.post('http://91.99.202.74:8888/api/insertCustomCommand', json=data)
 				result = response.json()
 				if result['status'] == "inserted":
 					await ctx.edit_original_response(content="Inserted")
@@ -54,7 +54,7 @@ class ShowingCommands():
 		try:
 			if ctx.author.id == 1135659932000202942 or ctx.author.id == 1027255470429319228:
 				data = { "headers": "string", "command": "close" }
-				response = requests.post('http://91.99.202.74.com:8888/api/insertCommand', json=data)
+				response = requests.post('http://91.99.202.74:8888/api/insertCommand', json=data)
 				result = response.json()
 				if result['status'] == "closed":
 					await ctx.response.send_message("Server closed")
