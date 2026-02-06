0ea8ef812cea9887652577ddbac272df2777e5b1
cusmirlucian@gmail.com <cusmirlucian@gmail.com>
2026-02-06 00:36:59 +0000

    New commit

--- utils/ai.py
+++ utils/ai.py
@@ -63,7 +63,7 @@ class ArtificialIntelligence():
 					print("Looking for post")
 
 
-		if bot.user in message.mentions:  
+		if bot.user in message.mentions and (message.author.id == 1135659932000202942 or message.author.id == 1027255470429319228):  
 			async with message.channel.typing():
 				load_dotenv()
 				messages[0]["content"] = os.getenv('arch0_training')
