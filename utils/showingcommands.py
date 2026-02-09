4a28a1cd91450ce0dcae10ba1845472554fc14c2
cusmirlucian@gmail.com <cusmirlucian@gmail.com>
2026-02-09 11:17:23 +0000

    New commit

--- utils/showingcommands.py
+++ utils/showingcommands.py
@@ -24,6 +24,9 @@ class ShowingCommands():
 				response = requests.post('http://91.99.202.74:8888/api/insertCommand', json=data)
 				result = response.json()
 				if result['status'] == "started":
+					channel = bot.get_channel(1469673733580128431)
+
+					channel.send("@here the server started! [ test message ]")
 					await ctx.response.send_message("Server Started")
 				else:
 					await ctx.response.send_message("There was an error, contact admin!")
