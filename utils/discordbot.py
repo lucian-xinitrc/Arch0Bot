d0dcf49dbf76d2c5ccd3bab97eb2aec1c66a6e29
cusmirlucian@gmail.com <cusmirlucian@gmail.com>
2026-02-17 20:18:32 +0000

    New commit

--- utils/discordbot.py
+++ utils/discordbot.py
@@ -45,8 +45,8 @@ class DiscordBot():
 	        channel = bot.get_channel(1473044902492246219)
 
 	        if channel:
-	        	if "login" not in msg or "register" not in msg or "/msg" not in msg:
-	            	await channel.send(f"**{config.Config().decrypt(author)}**: {config.Config().decrypt(msg)}")
+				if "login" not in msg or "register" not in msg or "/msg" not in msg:
+					await channel.send(f"**{config.Config().decrypt(author)}**: {config.Config().decrypt(msg)}")
 
 	
 	async def on_ready():
