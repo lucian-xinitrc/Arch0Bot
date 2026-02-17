e368d3b8f481b9256c96741081b15e07ec658992
cusmirlucian@gmail.com <cusmirlucian@gmail.com>
2026-02-17 20:34:05 +0000

    New commit

--- utils/discordbot.py
+++ utils/discordbot.py
@@ -43,13 +43,11 @@ class DiscordBot():
 
 	        author, msg = row
 	        channel = bot.get_channel(1473044902492246219)
-			
-			if channel:
+	        if channel:
 	 			if all(word not in msg for word in ["login", "register", "msg"]):
         			await channel.send(
             			f"**{config.Config().decrypt(author)}**: {config.Config().decrypt(msg)}"
         			)
-
 	
 	async def on_ready():
 		activity = disnake.Game(name="Arch BTW!")
