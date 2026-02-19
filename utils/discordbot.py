dbc0a596754c7a74727767707e868190a3d7d12f
cusmirlucian@gmail.com <cusmirlucian@gmail.com>
2026-02-19 22:03:14 +0000

    New commit

--- utils/discordbot.py
+++ utils/discordbot.py
@@ -53,9 +53,11 @@ class DiscordBot():
 	        	if check:
 	        		await channel.send(f"**{config.Config().decrypt(author)}**: {config.Config().decrypt(msg)}")
 
+	watcher.start()
 	@sc.ShowingCommands().prompt.event
 	async def on_ready():
 		activity = disnake.Game(name="Arch BTW!")
 		
 		await sc.ShowingCommands().prompt.change_presence(status=disnake.Status.idle, activity=activity)
 
+	
