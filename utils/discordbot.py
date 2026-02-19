9733bf202de816718f43c8055b8aeb5bd1773857
cusmirlucian@gmail.com <cusmirlucian@gmail.com>
2026-02-19 21:48:57 +0000

    New commit

--- utils/discordbot.py
+++ utils/discordbot.py
@@ -57,7 +57,7 @@ class DiscordBot():
 	@sc.ShowingCommands().prompt.event
 	async def on_ready():
 		activity = disnake.Game(name="Arch BTW!")
-		
+		watcher.start()
 		await sc.ShowingCommands().prompt.change_presence(status=disnake.Status.idle, activity=activity)
 
-	watcher.start()
+	
