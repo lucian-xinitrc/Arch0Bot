588ebdfffcd26cc635d4124feebde606c0222c7d
lucian-xinitrc <cusmirlucian@gmail.com>
2026-04-02 15:52:29 +0000

    New commit

--- utils/ai.py
+++ utils/ai.py
@@ -98,7 +98,7 @@ class ArtificialIntelligence():
 	async def faq(inter, ctx, message):
 		rulesFaq = [{"role": "system", "content": os.getenv('rules') }]
 		try:
-			guild = message.guild
+			guild = inter.guild
 			server1 = prompt.get_guild(1468746116332785777)
 			if guild == server1:
 				await ctx.response.defer()
